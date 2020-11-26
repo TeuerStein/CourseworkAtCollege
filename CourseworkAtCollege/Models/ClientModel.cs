a141e79d343ed16401b37ab36f58df0cbf81275f
TeuerStein <xenkai123456789@gmail.com>
2020-11-26 19:24:45 +0100

    Was getted all of objects from database into array. Was added all of this objects into dataGridView

--- CourseworkAtCollege/Models/ClientModel.cs
+++ CourseworkAtCollege/Models/ClientModel.cs
@@ -10,6 +10,8 @@ namespace CourseworkAtCollege
     {
 
         // Основні змінні класа
+        public int clientId;
+
         public string firstName;
 
         public string lastName;
@@ -23,5 +25,28 @@ namespace CourseworkAtCollege
         public string dateOfTheEndOfTheContract;
 
         public string typeOfCar;
+
+        public Client(
+             int clientIdFromConstructor,
+             string firstNameFromConstructor,
+             string lastNameFromConstructor,
+             string fatherNameFromConstructor,
+             string passportDataFromConstructor,
+             string phoneNumberFromConstructor,
+             string dateOfTheEndOfTheContractFromConstructor,
+             string typeOfCarFromConstructor
+         )
+        {
+            // Конструктор класа
+
+            this.clientId = clientIdFromConstructor;
+            this.firstName = firstNameFromConstructor;
+            this.lastName = lastNameFromConstructor;
+            this.fatherName = fatherNameFromConstructor;
+            this.passportData = passportDataFromConstructor;
+            this.phoneNumber = phoneNumberFromConstructor;
+            this.dateOfTheEndOfTheContract = dateOfTheEndOfTheContractFromConstructor;
+            this.typeOfCar = typeOfCarFromConstructor;
+        }
     }
 }
--- CourseworkAtCollege/Views/AllInfoAboutOrders.cs
+++ CourseworkAtCollege/Views/AllInfoAboutOrders.cs
@@ -7,18 +7,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace CourseworkAtCollege
 {
     public partial class AllInfoAboutOrders : Form
     {
+
         public AllInfoAboutOrders()
         {
             InitializeComponent();
+
+            GettingAllObjectsFromDataBase();
         }
 
         private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs eventArgs)
         {
+
+        }
+
+        private void dataGridView(string[] objectFromDataBase)
+        {
+            dataGridView1.Rows.Add(objectFromDataBase);
+        }
+
+        private void GettingAllObjectsFromDataBase()
+        {
+            DB dataBase = new DB();
+            dataBase.openConnection();
+
+            try
+            {
+                string commandForDataBase = "select * from 'client';";
+                MySqlCommand command = new MySqlCommand(commandForDataBase, dataBase.getConnection());
+
+                MySqlDataReader sqlReader = command.ExecuteReader();
+
+                List<string[]> objectsFromDataBase = new List<string[]>();
+
+                while (sqlReader.Read())
+                {
+                    objectsFromDataBase.Add(new string[8]);
+
+                    objectsFromDataBase[objectsFromDataBase.Count - 1][0] = sqlReader[0].ToString();
+                    objectsFromDataBase[objectsFromDataBase.Count - 1][1] = sqlReader[1].ToString();
+                    objectsFromDataBase[objectsFromDataBase.Count - 1][2] = sqlReader[2].ToString();
+                    objectsFromDataBase[objectsFromDataBase.Count - 1][3] = sqlReader[3].ToString();
+                    objectsFromDataBase[objectsFromDataBase.Count - 1][4] = sqlReader[4].ToString();
+                    objectsFromDataBase[objectsFromDataBase.Count - 1][5] = sqlReader[5].ToString();
+                    objectsFromDataBase[objectsFromDataBase.Count - 1][6] = sqlReader[6].ToString();
+                    objectsFromDataBase[objectsFromDataBase.Count - 1][7] = sqlReader[7].ToString();
+                }
+
+                sqlReader.Close();
+
+                try
+                {
+                    foreach (string[] singleObjectFromDataBaseForEach in objectsFromDataBase)
+                        dataGridView(singleObjectFromDataBaseForEach);
+                } catch
+                {
+                    Console.WriteLine("You have problems with dataGridView");
+                }
+
+            } catch(MySqlException exception) {
+                Console.WriteLine(exception.Message);
+            } finally {
+                dataBase.closeConnection();
+            }
 
         }
 
